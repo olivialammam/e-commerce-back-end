fbc443c254664d60ca9484d478db7bfc2862ffdf
Olivia Lammam <olivia.l7475@gmail.com>
2022-06-06 03:23:20 -0700

    error

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+let sequelize;
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
